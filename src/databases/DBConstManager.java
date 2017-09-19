df7448d799b575d13186a6b5767b720bcc8b9530
yjham2002 <yjham2002@gmail.com>
2017-09-19 11:17:25 +0900

    Connection Pool bug fixed

--- src/databases/DBConstManager.java
+++ src/databases/DBConstManager.java
@@ -1,27 +1,44 @@
 package databases;
 
-import databases.exception.NothingToTakeException;
+import constants.ConstProtocol;
 import models.DataMap;
 
 import java.sql.*;
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * @author 함의진
- * @version 1.0.0
- * MySQL 데이터베이스 연결 컨스턴트를 정의하고 연결 작업을 수행하기 위한 클래스
- * Jul-21-2017
+ * Created by a on 2017-04-03.
  */
-public class DBConstManager{
+public class DBConstManager {
 
     protected Connection connection = null;
     protected Statement st = null;
 
-    public static boolean autoConnect = true;
+    public static final String CONNECTOR = "jdbc";
+    public static final String DBMS = "mysql";
+//    public static final String HOST = "1.201.142.86";
+    public static final String HOST = "localhost";
+    public static final String PORT = "3306";
+    public static final String DBNAME = "sohatechfarmdb";
+    public static final String USERNAME = "sohatechfarmdb";
+    public static final String PASSWORD = "1!sohatechfarmdb";
+    public boolean autoConnect = true;
 
-    public static final String STANDARD_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public String getConnectionInfo(){
+        return CONNECTOR + ":" + DBMS + "://" + HOST + ":" + PORT + "/" + DBNAME + "?useUnicode=yes&amp;characterEncoding=UTF-8&amp;autoReconnect=" + autoConnect;
+    }
+
+    protected boolean debug = false;
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
 
     public boolean setAutoConnect(boolean value){
         this.autoConnect = value;
@@ -30,10 +47,8 @@ public class DBConstManager{
 
     // TODO PrimaryKey Constraint Violation
     public int getLastInsertId(String table){
-        DBConnectionPool db = DBConnectionPool.getInstance();
         try {
-            connection = db.getConnection();
-
+            connection = DriverManager.getConnection( getConnectionInfo() , USERNAME, PASSWORD);
             st = connection.createStatement();
             String sql = "SELECT LAST_INSERT_ID() AS number FROM " + table + " LIMIT 1";
             ResultSet rs = st.executeQuery(sql);
@@ -49,21 +64,24 @@ public class DBConstManager{
             rs.close();
             st.close();
 
+            connection.close();
+
             return res;
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("Error Handled :: getLastInsertId");
             return 1;
-        }finally {
-            db.freeConnection(connection);
         }
     }
 
+
     public List<DataMap> getList(String sql){
+        if(debug){
+            System.out.println("getList");
+        }
         List<DataMap> list = new Vector<>();
-        DBConnectionPool db = DBConnectionPool.getInstance();
         try{
-            connection = db.getConnection();
+            connection = DriverManager.getConnection( getConnectionInfo() , USERNAME, PASSWORD);
             st = connection.createStatement();
             ResultSet rs = st.executeQuery(sql);
             ResultSetMetaData metaData = rs.getMetaData();
@@ -81,128 +99,108 @@ public class DBConstManager{
             rs.close();
             st.close();
 
-        }catch(Exception e){
-            System.out.println("Error Handled :: getList");
-        }finally {
-            db.freeConnection(connection);
+        }catch(SQLException e){
+            e.printStackTrace();
         }
 
         return list;
     }
 
-    public DataMap getRow(String sql) throws NothingToTakeException {
-        DataMap dataMap = new DataMap();
-        DBConnectionPool db = DBConnectionPool.getInstance();
-        try {
-            connection = db.getConnection();
-            st = connection.createStatement();
-
-            ResultSet rs = st.executeQuery(sql);
-            ResultSetMetaData metaData = rs.getMetaData();
-
-            int cols = metaData.getColumnCount();
-
-            rs.next();
-            for(int i = 1; i <= cols; i++){
-                Object obj = rs.getObject(i);
-                if(obj instanceof Timestamp){
-                    obj = new SimpleDateFormat(STANDARD_DATE_FORMAT).format(obj).toString();
-                }
-                dataMap.put(metaData.getColumnLabel(i), obj);
-            }
-
-            rs.close();
-            st.close();
-
-            return dataMap;
-        }catch (SQLException e){
-            throw new NothingToTakeException();
-        }catch(Exception e){
-            System.out.println("Error Handled :: getRow");
-            return dataMap;
-        }finally {
-            db.freeConnection(connection);
-        }
-    }
 
     public boolean execute(String sql){
+        if(debug){
+            System.out.println("execute :: " + sql.replaceAll("\n", ""));
+        }
         boolean retVal = false;
-        DBConnectionPool db = DBConnectionPool.getInstance();
+
         try {
-            connection = db.getConnection();
+            connection = DriverManager.getConnection(getConnectionInfo(), USERNAME, PASSWORD);
             st = connection.createStatement();
             retVal = st.execute(sql);
-            return  retVal;
+            st.close();
+            connection.close();
+
+            return retVal;
+        }catch (DataTruncation ee){
+            ee.printStackTrace();
+            return false;
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("Error Handled :: execute");
             return false;
-        }finally {
-            db.freeConnection(connection, st);
         }
     }
 
-    public long getNumber(String sql, String column) throws NothingToTakeException {
-        DBConnectionPool db = DBConnectionPool.getInstance();
+    public long getNumber(String sql, String column){
+        if(debug){
+            System.out.println("getNumber");
+        }
         try {
-            connection = db.getConnection();
+            connection = DriverManager.getConnection( getConnectionInfo() , USERNAME, PASSWORD);
             st = connection.createStatement();
             ResultSet rs = st.executeQuery(sql);
 
             long res = 0;
 
-            int isNone = 0;
-
             while(rs.next()){
-                isNone++;
                 res = rs.getLong(column);
             }
-
-            if(isNone == 0) throw new NothingToTakeException();
 
             rs.close();
             st.close();
 
+            connection.close();
+
             return res;
         }catch(Exception e){
+            e.printStackTrace();
             System.out.println("Error Handled :: getNumber");
             return 0;
-        }finally {
-            db.freeConnection(connection);
         }
     }
 
     public List<String> getStrings(String sql, String... column){
-        List<String> returnList = new Vector<>();
-        DBConnectionPool db = DBConnectionPool.getInstance();
+        if(debug){
+            System.out.println("getStrings :: " + sql.replaceAll("\n", ""));
+        }
+        List<String> phones = new Vector<>();
+
         try {
-            connection = db.getConnection();
+            connection = DriverManager.getConnection( getConnectionInfo() , USERNAME, PASSWORD);
             st = connection.createStatement();
             ResultSet rs = st.executeQuery(sql);
 
             String res = "";
 
             while(rs.next()){
-                for(String col : column) returnList.add(rs.getString(col));
+                for(String col : column) {
+                    if(rs.getString(col) == null){
+                        phones.add(null);
+                    }else {
+                        if (!rs.getString(col).equals("--")) phones.add(rs.getString(col));
+                    }
+                }
             }
 
             rs.close();
             st.close();
 
-            return returnList;
+            connection.close();
+
+            return phones;
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("Error Handled :: getStrings");
-            return returnList;
-        }finally {
-            db.freeConnection(connection);
+            return phones;
         }
     }
 
     public String getString(String sql, String column){
-        DBConnectionPool db = DBConnectionPool.getInstance();
+        if(debug){
+            System.out.println("getString");
+        }
         try {
-            connection = db.getConnection();
+            connection = DriverManager.getConnection( getConnectionInfo() , USERNAME, PASSWORD);
             st = connection.createStatement();
             ResultSet rs = st.executeQuery(sql);
 
@@ -215,13 +213,13 @@ public class DBConstManager{
             rs.close();
             st.close();
 
+            connection.close();
+
             return res;
         }catch(Exception e){
             e.printStackTrace();
             System.out.println("Error Handled :: getString");
             return null;
-        }finally {
-            db.freeConnection(connection);
         }
     }
 
--- src/databases/DBManager.java
+++ src/databases/DBManager.java
@@ -1,16 +1,26 @@
 package databases;
 
-import java.sql.SQLException;
+import pojo.RealtimePOJO;
+import redis.RedisManager;
+
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author 함의진
- * @version 1.0.0
- * MySQL 데이터베이스 연결을 위한 싱글턴 클래스
- * Jul-21-2017
  */
 public class DBManager extends DBConstManager {
 
-    private static DBManager instance;
+    public static DBManager instance;
+
+    public static void setInstance(DBManager instance) {
+        DBManager.instance = instance;
+    }
 
     public static DBManager getInstance(){
         if(instance == null) instance = new DBManager();
@@ -18,9 +28,9 @@ public class DBManager extends DBConstManager {
     }
 
     private DBManager(){
-        DBConnectionPool db = DBConnectionPool.getInstance();
         try {
-            connection = db.getConnection();
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection( getConnectionInfo() , USERNAME, PASSWORD);
             st = connection.createStatement();
         } catch (SQLException se1) {
             se1.printStackTrace();
@@ -30,8 +40,49 @@ public class DBManager extends DBConstManager {
             try {
                 if (st != null) st.close();
             } catch (SQLException se2) {}
-            db.freeConnection(connection);
+            try {
+                if (connection != null) connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
         }
     }
+
+//    public boolean migrateFromRedis(){
+//        try {
+//            String pattern = RedisManager.getYYMMDDwithPostfix("-");
+//
+//            List<RealtimePOJO> list = RedisManager.getInstance().getList(pattern, RealtimePOJO.class);
+//
+//            Collections.sort(list, (o1, o2) -> {
+//                long t1 = Long.parseLong(o1.getRedisTime());
+//                long t2 = Long.parseLong(o2.getRedisTime());
+//                if(t1 == t2) return 0;
+//                else if(t1 > t2) return 1;
+//                else return -1;
+//            });
+//
+//            long maxTime = getNumber("SELECT MAX(redisTime) AS num FROM tblRealTimeData", "num");
+//
+//            int count = 0;
+//            for (RealtimePOJO pojo : list) {
+//                if(Long.parseLong(pojo.getRedisTime()) > maxTime) {
+//                    count++;
+//                    String sql = pojo.getInsertSQL();
+//                    execute(sql);
+//                }
+//            }
+//            System.out.println("[Migrating DB Data from REDIS to MySQL] " + count++ + "/" + list.size() + " has newly inserted.");
+//        }catch(Exception e){
+//            System.out.println("Error handled :: migrateFromRedis");
+//            return false;
+//        }finally {
+//            long delCount = getNumber("SELECT COUNT(*) AS num FROM tblRealTimeData WHERE (DATE_SUB(NOW(), INTERVAL 2 MONTH) > regDate)", "num");
+//            execute("DELETE FROM tblRealTimeData WHERE (DATE_SUB(NOW(), INTERVAL 2 MONTH) > regDate)");
+//            System.out.println("[Expiration Check] " + delCount + " items which are expired have deleted from MySQL DB");
+//        }
+//
+//        return true;
+//    }
 
 }
