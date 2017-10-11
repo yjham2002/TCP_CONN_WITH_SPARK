fc125582842393a208dd586971ee4771a2396532
yjham2002 <yjham2002@gmail.com>
2017-10-11 13:59:20 +0900

    TimeData added on Logging

--- src/utils/Log.java
+++ src/utils/Log.java
@@ -1,22 +1,29 @@
 package utils;
 
+import java.util.Calendar;
+
 public class Log {
-    public static final String ERR = "[WARN] ";
-    public static final String INFO = "[INFO] ";
+    public static final String ERR = "[WARN]";
+    public static final String INFO = "[INFO]";
 
     public static void e(String msg){
-        System.err.println(ERR + msg);
+        System.err.println(logPrefix(ERR) + msg);
     }
 
     public static void i(String msg){
-        System.out.println(INFO + msg);
+        System.out.println(logPrefix(INFO) + msg);
     }
 
     public static void e(Object msg){
-        System.err.println(ERR + msg.toString());
+        System.err.println(logPrefix(ERR) + msg.toString());
     }
 
     public static void i(Object msg){
-        System.out.println(INFO + msg.toString());
+        System.out.println(logPrefix(INFO) + msg.toString());
+    }
+
+    private static String logPrefix(String prefix){
+        final String str = String.format("%s %s - [%s] ", prefix, Calendar.getInstance().getTime().toString(), Thread.currentThread().getName());
+        return str;
     }
 }
